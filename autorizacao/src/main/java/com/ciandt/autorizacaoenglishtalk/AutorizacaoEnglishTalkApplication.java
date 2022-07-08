90c59187a7cdcb86866816c93083a4a3e938e661
Dara S. Mariano <darasouza@ciandt.com>
2022-07-08 16:18:32 -0300

    [STARTER] - Inicializacao do springboot

---
+++ autorizacao/src/main/java/com/ciandt/autorizacaoenglishtalk/AutorizacaoEnglishTalkApplication.java
@@ -0,0 +1,13 @@
+package com.ciandt.autorizacaoenglishtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutorizacaoEnglishTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutorizacaoEnglishTalkApplication.class, args);
+	}
+
+}
